d812692e8613305a02a42aee7680b08e36e53785
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-10-13 22:01:06 +0100

    Added first validation tests

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -8,4 +8,16 @@ describe Restaurant do
 
 	it { should respond_to(:name) }
 	it { should respond_to(:description) }
+
+	it { should be_valid }
+
+	describe "when name is not present" do
+		before { @restaurant.name = " "}
+		it { should_not be_valid }
+	end
+
+	describe "when description is not present" do
+		before { @restaurant.description = " "}
+		it { should_not be_valid }
+	end
 end
